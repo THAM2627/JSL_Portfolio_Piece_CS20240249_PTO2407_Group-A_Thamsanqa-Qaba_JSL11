5f9e8e09ad3d3639386b07b65a4e515974417270
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-02-21 16:24:17 +0200

    display boards function update

--- index.js
+++ index.js
@@ -105,19 +105,12 @@ function displayBoards(boards) {
     boardElement.textContent = board;
     boardElement.classList.add("board-btn");
     boardElement.addEventListener('click', () => {
-      boardElement.classList.addEventListener('click', () => {
-        elements.headerBoardName.textContent = board;
-        filterAndDisplayTasksByBoard(board);
-        activeBoard = board //assigns active board
-        localStorage.setItem("activeBoard", JSON.stringify(activeBoard))
-        styleActiveBoard(activeBoard)
-      });
-      boardsContainer.appendChild(boardElement);
       elements.headerBoardName.textContent = board;
       filterAndDisplayTasksByBoard(board);
       activeBoard = board //assigns active board
       localStorage.setItem("activeBoard", JSON.stringify(activeBoard))
       styleActiveBoard(activeBoard)
+
     });
     boardsContainer.appendChild(boardElement);
   });
